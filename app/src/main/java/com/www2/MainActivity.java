2aa58159b64447a6df5ed5bcbe3ac37012111490
秋天的稻谷 <516793413@qq.com>
2017-01-13 14:22:36 +0800

    Update MainActivity.java

--- app/src/main/java/com/www2/MainActivity.java
+++ app/src/main/java/com/www2/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     public  int a;
+    private  int b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
